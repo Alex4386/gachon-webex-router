fe54584a0bc254941d43fe7dc4641603820ee0c5
Alex4386 <psh010209@gmail.com>
2020-04-15 00:41:55 +0900

    update: footer

--- common/footer.php
+++ common/footer.php
@@ -4,5 +4,6 @@
                 <li class="list-inline-item"><a href="https://gachon.ac.kr">가천대학교 홈페이지</a></li>
                 <li class="list-inline-item"><a href="https://sw.gachon.ac.kr">가천대학교 소프트웨어학과</a></li>
             </ul>
-            <p class="copyright">현재 개발중인 기능으로 완벽히 지원되지 않습니다.<br>동명이인의 교수님이 계신 경우, 먼저 등록된 교수님이 우선적으로 연결됩니다.<br>인증절차 이후 연결됩니다.</p>
+            <p class="copyright">현재 개발중인 기능으로 완벽히 지원되지 않습니다.<br>동명이인의 교수님이 계신 경우, 먼저 등록된 교수님이 우선적으로 연결됩니다.<br>해당 교수님 아이디에 대한 인증절차 이후 등록이 연결됩니다.</p>
+            <!-- 개발자? 누군지 궁금하다면 대답해 주는게 인지상정인가? Distributed under MIT License, Source code of this software is available at https://github.com/Alex4386/gachon-webex-router -->
         </footer>
